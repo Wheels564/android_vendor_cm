5c8b997039568bc88bbfc274fd3c55e181957c21
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2020-10-17 11:07:00 -0400

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master/lineage-build-targets | sed -e 's/#.*$//' | grep cm-14.1 | awk '{printf "lineage_%s-%s\n", $1, $2}')
+do
+    add_lunch_combo $combo
+done
+add_lunch_combo lineage_mb886
+add_lunch_combo lineage_xt907_jbbl
+add_lunch_combo lineage_xt925_jbbl
+add_lunch_combo lineage_xt926_jbbl
